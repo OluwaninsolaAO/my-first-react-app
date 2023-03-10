7ed436a85793204965daef555da4e5a2a3108998
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-11 00:20:34 +0100

    Add counter to navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
+    
     render() { 
         return (
             <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">
+                        Navbar <span className="badge badge-pill badge-secondary bg-secondary">{this.props.totalCounters}</span>
+                        </a>
                 </div>
             </nav>
         );
