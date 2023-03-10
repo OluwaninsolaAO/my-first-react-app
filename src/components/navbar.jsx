27b9f92df902fc11ea78c2fe9edf663c27736ee5
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-11 00:28:15 +0100

    Refactor NavBar to Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    
-    render() { 
-        return (
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        Navbar <span className="badge badge-pill badge-secondary bg-secondary">{this.props.totalCounters}</span>
-                        </a>
-                </div>
-            </nav>
-        );
-    }
+const NavBar = (props) => {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Navbar <span className="badge badge-pill badge-secondary bg-secondary">{props.totalCounters}</span>
+                    </a>
+            </div>
+        </nav>
+    );
 }
  
 export default NavBar;
