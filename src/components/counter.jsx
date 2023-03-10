f52aacb9dc7cea7501b1362e3db25cf6986aaddc
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-10 23:32:34 +0100

    Refactor Counter and Counters to have a Single Source of Truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,
-        // tags: [ 'tag1', 'tag2', 'tag3' ]
-    };
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 })
-    }
-
     render() {        
         return (            
                 <div>
                     <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                    <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                    <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increment</button>
+
+                    <button
+                    className="btn btn-danger btn-sm m-2" 
+                    onClick={() => this.props.onDelete(this.props.counter.id)}>
+                        Delete
+                    </button>
 
                     {/* <ul>
                         { this.state.tags.map( tag => <li key={tag}>{ tag }</li>) }
@@ -25,12 +22,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 bg-";
-        classes += this.state.value === 0 ? "warning text-black" : "primary";
+        classes += this.props.counter.value === 0 ? "warning text-black" : "primary";
         return classes;
     }
 
     formatCount () {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? "Zero" : value;
     }
 }
