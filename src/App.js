7733625c67f07706f3859b7d553afe13bacbd6c5
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-10 23:55:54 +0100

    Change App component to use class component instead of function component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React, { Component } from 'react';
 import NavBar from './components/navbar';
-import './App.css';
 import Counters from './components/counters';
 
-function App() {
-  return (
-    <>
-    <NavBar />
-    <main className='container'>
-      <Counters />
-    </main>
-    </>
-  );
+class App extends Component {
+  render() { 
+    return (
+      <>
+        <NavBar />
+        <main className='container'>
+          <Counters />
+        </main>
+      </>
+    );
+  }
 }
-
+ 
 export default App;
